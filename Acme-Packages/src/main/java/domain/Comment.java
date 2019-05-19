e649129b9e9152c2b53fc26b749f1a2081ed4588
Adrián Mora Carrero <adrian_mc_97@hotmail.com>
2019-05-19 16:02:00 +0200

    Domain model java updated

---
+++ Acme-Packages/src/main/java/domain/Comment.java
@@ -0,0 +1,56 @@
+
+package domain;
+
+import java.util.Date;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+
+import org.hibernate.validator.constraints.NotBlank;
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Access(AccessType.PROPERTY)
+public class Comment extends DomainEntity {
+
+	private Date	moment;
+	private String	comment;
+	private String	username;
+
+
+	@NotNull
+	@Past
+	@Temporal(TemporalType.TIMESTAMP)
+	@DateTimeFormat(pattern = "dd/MM/yyyy HH:mm:ss")
+	public Date getMoment() {
+		return this.moment;
+	}
+
+	public void setMoment(final Date moment) {
+		this.moment = moment;
+	}
+
+	@NotBlank
+	public String getComment() {
+		return this.comment;
+	}
+
+	public void setComment(final String comment) {
+		this.comment = comment;
+	}
+
+	@NotBlank
+	public String getUsername() {
+		return this.username;
+	}
+
+	public void setUsername(final String username) {
+		this.username = username;
+	}
+
+}
