03c9785417990c211f8dbda03a70daf6a61ca122
JMdelosReyes <38224590+JMdelosReyes@users.noreply.github.com>
2019-05-26 08:52:27 +0200

    Queries del dashboard

--- Acme-Packages/src/main/java/repositories/AdministratorRepository.java
+++ Acme-Packages/src/main/java/repositories/AdministratorRepository.java
@@ -40,6 +40,7 @@ public interface AdministratorRepository extends JpaRepository<Administrator, In
 	@Query("select stddev(c.score) from Carrier c")
 	Double stddevScoreCarriers();
 
+	// TODO ESTO ENTIENDO QUE SE REFIERE A LA NOTA DE LAS EVALUACIONES, NO AL NÚMERO DE LAS MISMAS
 	//The average, the minimum, the maximum, and the standard deviation of evaluations made by customers.
 	@Query("select avg(c.evaluations.size) from Customer c")
 	Double avgEvaluationByCustomer();
@@ -75,7 +76,8 @@ public interface AdministratorRepository extends JpaRepository<Administrator, In
 	Collection<Carrier> top3CarriersWithHigherScore();
 
 	//Top-5 most visited towns.
-	//TODO
+	//TODO 
+	//select t.name, count(tt) from TraverseTown tt join tt.town t group by tt.town order by count(tt) desc;
 
 	//The ratio of empty versus non-empty finders.
 	@Query("select (count(f2)/(select count(f1) from Finder f1)+0.0) from Finder f2 where f2.offers.size=0")
@@ -85,10 +87,10 @@ public interface AdministratorRepository extends JpaRepository<Administrator, In
 	Double RatioNonEmptyFinders();
 
 	//The listing of auditors who have got at least 10% of issues closed above the average. 
-	//TODO
+	//TODO No la he podido probar
+	//select a from Auditor a join a.issues i where ((select count(i2) from Auditor a2 join a2.issues i2 where a2.id=a.id and i2.closed=1)/(a.issues.size)+0.0)>(select avg(1.0*(select count(i3) from Auditor a4 join a4.issues i3 where i3.closed=1)) from Auditor a3)*1.1;
 
 	//The ratio of closed versus non-closed issues.
-
 	@Query("select count(i)/(select count(i2) from Issue i2)+0.0 from Issue i where i.closed=0")
 	Double ratioNonClosedIssue();
 	@Query("select count(i)/(select count(i2) from Issue i2)+0.0 from Issue i where i.closed=1")
